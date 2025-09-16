27670de5e51c1263ada3bfa77fdb641879bdb824
Jhofran Briceño <jhofran.dev@outlook.com>
2025-09-16 17:00:29 -0500

    feat(clients): add search input and client list components
    Implement search functionality with SearchInput component and display client list in ClientsView

---
+++ src/components/clients/SearchInput.tsx
@@ -0,0 +1,32 @@
+export default function SearchInput() {
+  return (
+    <form className="max-w-sm mx-4">
+      <div className="relative mb-4">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-400 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-2 ps-10 text-sm text-gray-500 border border-gray-200 rounded-lg bg-gray-50 focus:ring-yellow-300 focus:border-yellow-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-300 dark:focus:border-yellow-300 placeholder:text-gray-400"
+          placeholder="Buscar clientes"
+          required
+        />
+      </div>
+    </form>
+  );
+}
--- src/views/client/ClientsView.tsx
+++ src/views/client/ClientsView.tsx
@@ -1,3 +1,135 @@
+import { Link } from "react-router-dom";
+import SearchInput from "../../components/clients/SearchInput";
+
 export default function ClientsView() {
-  return <div>ClientsView</div>;
+  return (
+    <>
+      <div className="py-4">
+        <SearchInput />
+        <Link
+          to="#"
+          className="block focus:outline-none text-white mx-4 bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm text-center px-4 py-2.5 dark:focus:ring-yellow-900"
+        >
+          Nuevo cliente
+        </Link>
+        <div className="flow-root mt-4">
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 dark:divide-gray-700"
+          >
+            <li className="px-4 py-4 sm:py-4">
+              <div className="flex items-center">
+                <div className="shrink-0">
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src="/docs/images/people/profile-picture-1.jpg"
+                    alt="Neil image"
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    Neil Sims
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $320
+                </div>
+              </div>
+            </li>
+            <li className="px-4 py-4 sm:py-4">
+              <div className="flex items-center ">
+                <div className="shrink-0">
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src="/docs/images/people/profile-picture-3.jpg"
+                    alt="Bonnie image"
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    Bonnie Green
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $3467
+                </div>
+              </div>
+            </li>
+            <li className="px-4 py-4 sm:py-4">
+              <div className="flex items-center">
+                <div className="shrink-0">
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src="/docs/images/people/profile-picture-2.jpg"
+                    alt="Michael image"
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    Michael Gough
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $67
+                </div>
+              </div>
+            </li>
+            <li className="px-4 py-4 sm:py-4">
+              <div className="flex items-center ">
+                <div className="shrink-0">
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src="/docs/images/people/profile-picture-4.jpg"
+                    alt="Lana image"
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    Lana Byrd
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $367
+                </div>
+              </div>
+            </li>
+            <li className="px-4 pt-4 pb-0 sm:pt-4">
+              <div className="flex items-center ">
+                <div className="shrink-0">
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src="/docs/images/people/profile-picture-5.jpg"
+                    alt="Thomas image"
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    Thomes Lean
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $2367
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
